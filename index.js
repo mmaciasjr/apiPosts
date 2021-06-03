939ea1efb7628442440f725ae441035688b7e6b0
mmaciasjr <mariomacias29@gmail.com>
2021-06-02 19:36:03 -0700

    added new user to db using postman.

--- index.js
+++ index.js
@@ -1,19 +1,44 @@
 const express = require('express');
 const app = express();
-const users = require('./posts.json')
+const posts = require('./posts.json');
+const fs = require('fs');
+//route created
+app.use(express.json());
+app.use(express.urlencoded());
 
-app.get('/', function (req, res) {
-    res.send('Posts.json assignment');
-});
+app.get('/', (req, res) => {
+    res.send('Whats up world?');
+})
+//get request
+app.get('/books', function (req, res) {
+    res.send('There are hella books in the store. Yee!!!');
+})
 
+//creating post route
 app.post('/', function (req, res) {
-    res.send('This is a post request')
+    res.send('this is a post request');
+})
+//fetch all posts
+app.get('/posts', (req, res) => {
+return res.json({ posts })
+});
+//create new post from clients request in order to save new user to existing db.
+app.post('/posts', (req, res) => {
+console.log(req.body.newPost);
+
+posts.push(req.body.newPost);
+
+let stringedData = JSON.stringify(posts, null, 2);
+fs.writeFile('users.json', stringedData, function (err) {
+    if (err ){
+        return res.status(500).json({message: 'err'})
+    }
+    })
+
+    return res.status(200).json({ message: "New post has been created"})
 })
 
-app.get('/users', (req, response) => {
-return res.json({ users })
-})
-
-app.listen(3000, function () {
+//
+app.listen(3000, function(){
     console.log('Server running on port 3000')
 })
